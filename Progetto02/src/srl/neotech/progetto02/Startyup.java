580f060afd87436d3f0888f53c5cdc19276db5ec
academy <academy@neotech.srl>
2022-10-26 19:52:32 +0200

    progetto02

---
+++ Progetto02/src/srl/neotech/progetto02/Startyup.java
@@ -0,0 +1,10 @@
+package srl.neotech.progetto02;
+
+public class Startyup {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Progetto02");
+	}
+
+}
