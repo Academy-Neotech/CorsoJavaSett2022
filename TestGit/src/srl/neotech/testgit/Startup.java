c88482274fa01838f6bca59271f60e1399c2e40e
academy <academy@neotech.srl>
2022-10-27 18:54:03 +0200

    modifica develop senza pulsante rosso

--- TestGit/src/srl/neotech/testgit/Startup.java
+++ TestGit/src/srl/neotech/testgit/Startup.java
@@ -6,6 +6,7 @@ public class Startup {
 		// TODO Auto-generated method stub
 		System.out.println("Test Git");
 		System.out.println("Test git 02");
+		System.out.println("Aggiungo develop senza ulsante rosso");
 	}
 
 }
