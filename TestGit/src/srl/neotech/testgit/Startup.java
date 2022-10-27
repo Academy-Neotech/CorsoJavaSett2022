f81d7f71ffbf09a4a482b255258e4652c6993d13
academy <academy@neotech.srl>
2022-10-27 18:36:14 +0200

    quarta commit

--- TestGit/src/srl/neotech/testgit/Startup.java
+++ TestGit/src/srl/neotech/testgit/Startup.java
@@ -5,6 +5,7 @@ public class Startup {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Test Git");
+		System.out.println("Test git 02");
 	}
 
 }
