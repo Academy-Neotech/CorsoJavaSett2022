910b5fadc925580f35648cbd15475bd14b56bae8
academy <academy@neotech.srl>
2022-12-05 21:41:26 +0100

    Spring IOC e Dependency Injection

--- MN_NomePRJ/src/main/java/srl/neotech/configuration/SpringBootTomcatApplication.java
+++ MN_NomePRJ/src/main/java/srl/neotech/configuration/SpringBootTomcatApplication.java
@@ -7,14 +7,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import srl.pippo.fuoriContext.Automobile;
+
 @EnableAutoConfiguration
-@SpringBootApplication(scanBasePackages = "srl.neotech")
+@SpringBootApplication(scanBasePackages = "srl.neotech, srl.pippo")
 public class SpringBootTomcatApplication  extends SpringBootServletInitializer{
 	  
 	public static void main(String[] args) {
 
+
+		Automobile auto=new Automobile();
+		auto.getMotore().setCilindrata(1550);
+		
+	
+		
         SpringApplication.run(SpringBootTomcatApplication.class, args);
- 
+
     }
 
     @Override
--- MN_NomePRJ/src/main/java/srl/neotech/controllers/MusicController.java
+++ MN_NomePRJ/src/main/java/srl/neotech/controllers/MusicController.java
@@ -1,14 +1,32 @@
 package srl.neotech.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import srl.pippo.fuoriContext.Automobile;
+
 @Controller
 public class MusicController {
-
+	
+	//IOC: Inversion of Control.
+	
+	@Autowired //voglio usare 
+	Automobile auto;
+	
+	//dimenticatevi le variabili livello di classe.
+	
+	
 	//Rotta
 	@GetMapping(value="prima_rotta")
+	//le chiamate sono serializzate....
+	
 	public String primaRotta() {
+		int contaOggetti=0;
+		auto.getMotore().setCilindrata(3000);
+		System.out.println(auto.getMotore().getCilindrata());
+		contaOggetti=contaOggetti+1;
+		System.out.println(contaOggetti);
 		return "test";
 	}
 	
